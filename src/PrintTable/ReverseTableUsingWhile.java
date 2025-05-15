e442fde0a1ba8d56bbfad8f1e93bd72e37c7d0db
GreeshmaMore@23 <shwetasgadekar@gmail.com>
2025-05-15 19:48:48 +0530

    Second commit to add reverse Table program

---
+++ src/PrintTable/ReverseTableUsingWhile.java
@@ -0,0 +1,29 @@
+package PrintTable;
+
+import java.util.Scanner;
+
+public class ReverseTableUsingWhile {
+
+	public static void main(String[] args) {
+			int i=10;
+			Scanner sc=new Scanner(System.in);
+			System.out.print("Enter a number to obtain its reverse table :");
+			int num=sc.nextInt();
+			
+			while(i>=0) {
+			System.out.print(i*num+" ");
+			i--;
+			//System.out.println("\n");
+			
+			}
+			
+			i=10;
+			System.out.print("\nEnter a number to obtain its reverse table :");
+			num=sc.nextInt();
+			do {
+				System.out.print(i*num+" ");
+				i--;
+			}while(i>=0);
+	}
+
+}
