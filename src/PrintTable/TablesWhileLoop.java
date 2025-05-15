fc26212fa3d82b0585fd965b78b41426468e7fa2
GreeshmaMore@23 <shwetasgadekar@gmail.com>
2025-05-15 19:58:36 +0530

    Third commit

---
+++ src/PrintTable/TablesWhileLoop.java
@@ -0,0 +1,27 @@
+package PrintTable;
+
+import java.util.Scanner;
+
+public class TablesWhileLoop {
+
+	public static void main(String[] args) {
+		int num;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number for while loop =");
+		num=sc.nextInt();
+		int i=1;
+		while(i<=10) {
+			System.out.print(i*num+" ");
+			i++;
+		}
+		// Table using do while loop:
+		int k=1;
+		System.out.println("\nEnter the number for do-while loop: =");
+		num=sc.nextInt();
+		do{
+			System.out.print(k*num+" ");
+			k++;
+		}while(k<=10);
+	}
+
+}
