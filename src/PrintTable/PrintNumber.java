a4bc409d89d2a0c65eddbbe1904fde6241f0e35f
GreeshmaMore@23 <shwetasgadekar@gmail.com>
2025-05-16 22:00:21 +0530

    print number program added

---
+++ src/PrintTable/PrintNumber.java
@@ -0,0 +1,24 @@
+package PrintTable;
+
+public class PrintNumber {
+
+	public static void main(String[] args) {
+	
+		//1. Print 1 to 100 number using while loop:
+		
+		int i=1;
+		while(i<=100) {
+			System.out.print(i+" ");
+			i++;
+		}
+		System.out.println("\n-------------------------------------------------------------------------------");
+		//2. Print 1 to 100 number using do-while loop:
+		int k=1;
+		do {
+			System.out.print(k+" ");
+			k++;
+		}while(k<=100);
+			
+	}
+
+}
